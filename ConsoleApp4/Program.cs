6c6a3670f7a49f6640708e2145af84b52cbb8264
s227398599 <s227398599@LAB1-11>
2023-03-29 15:23:39 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Number \t Square");
+            for (int i = 1; i <= 100; i++)
+            {
+                int square = i * i;
+                Console.WriteLine("{0} \t {1}", i, square);
+            }
+            Console.ReadLine();
+        }
+    }
+    }
+
